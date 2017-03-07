9981a50d8734e621da404dd8092fb84499a41862
Tom McMahon <thmcmahon@gmail.com>
2017-03-08 09:45:04 +1100

    Updated the income tax function and removed the older functions

--- R/income_tax.R
+++ R/income_tax.R
@@ -4,17 +4,13 @@
 #' @param income Numeric. Gross income.
 #' @param rates Numeric vector. Rate for each bracket.
 #' @param brackets Numeric vector. Top of each tax bracket.
-#' @param super Logical. Whether to calculate superannuation tax or income tax
-#'   liability.
 #' 
 #' @examples 
 #' # Calculate tax liability for taxpayer with gross income of 50,000
 #' income_tax(50000)
-income_tax <- function(income,
-                       rates = c(0, .19, .325, .37, .45),
-                       brackets = c(18200,37000,80000,180000,Inf),
-                       super = FALSE) {
-    tax <- mcalc(income, rates = rates, brackets = brackets, category = "tax")
-    # If it's a superannuation tax then we need to get nine per cent
-    ifelse(super == FALSE, return(tax), return(tax * .09))
+income_tax <- function(income, rates = c(0, .19, .325, .37, .45),
+                       brackets = c(18200, 37000, 80000, 180000, Inf)) {
+  income_by_bracket <- diff(c(0, pmin(income, brackets)))
+  sum(income_by_bracket * rates)
 }
+  
