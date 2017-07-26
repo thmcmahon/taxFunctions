3862e54d644fdc2e637c2f6e64b9400986f66ab8
Tom McMahon <thmcmahon@gmail.com>
2017-07-26 13:39:18 +1000

    fixed lito

--- R/lito.R
+++ R/lito.R
@@ -2,24 +2,23 @@
 #' tax offset that reduces individuals' tax owed.
 #'
 #' @param income Numeric. Gross income.
-#' @param amount Numeric. The untapered low income tax offset amount.
-#' @param taper_brackets Numeric vector.
+#' @param value Numeric. The full value of the offset.
+#' @param taper_start Numeric. Income level from which to start tapering.
 #'
 #' @export
 #'
 #' @examples
 #' lito(18000)
-lito <- function(income, amount = 445, taper_brackets = c(37000, 66667),
-                 taper_rate = .015) {
-    if (income > max(taper_brackets)) {
-        lito <- 0
-    } else if (income < min(taper_brackets)) {
-        lito <- amount
+lito <- function(income, value = 445, taper_start = 37000,
+                  taper_rate = .015) {
+  if (income < taper_start) {
+    return(value * -1)
+  } else {
+    tapered_lito <- (value - (income - taper_start) * taper_rate) * -1
+    if (tapered_lito < 0) {
+      return(tapered_lito)
     } else {
-        lito <- amount - (income - min(taper_brackets)) * taper_rate
+      return(0)
     }
-    if (lito <= 0 & refundable == FALSE) {
-        lito <- 0
-    }
-    return(lito)
+  }
 }
